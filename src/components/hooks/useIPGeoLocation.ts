0e42592a5a091fef38437864a330d1a3824c9946
Patryk Baranek <cien2460045@gmail.com>
2023-02-18 11:48:14 +0100

    add validation ip

--- src/components/hooks/useIPGeoLocation.ts
+++ src/components/hooks/useIPGeoLocation.ts
@@ -5,46 +5,49 @@ import { IPFY } from '../../types/ipfy-types';
 const IP_GEOLOCATION_COUNTRY_API = 'https://geo.ipify.org/api/v2/country,city';
 
 interface IPGeoLocationProps {
-  ipAddress: string | undefined;
+	ipAddress: string | undefined;
 }
 
 export const useIPGeoLocation = ({ ipAddress }: IPGeoLocationProps) => {
-  const [result, setResult] = useState<IPFY | undefined>(undefined);
+	const [result, setResult] = useState<IPFY | undefined>(undefined);
 
-  const validateIPAdress = (): RegExpMatchArray | null => {
-    if (ipAddress === undefined) return null;
-    const regexPattern = new RegExp('^(?:[0-9]{1,3}.){3}[0-9]{1,3}$');
+	const isIPAddressValid = (): RegExpMatchArray | null => {
+		if (ipAddress === undefined) return null;
+		const regexPattern = new RegExp('^(?:[0-9]{1,3}.){3}[0-9]{1,3}$');
 
-    console.log(ipAddress.match(regexPattern));
+		console.log(ipAddress.match(regexPattern));
 
-    return ipAddress.match(regexPattern);
-  };
+		return ipAddress.match(regexPattern);
+	};
 
-  const fetchIPGeoLocation = async (): Promise<void> => {
-    if (ipAddress === undefined) return;
+	const fetchIPGeoLocation = async (): Promise<void> => {
+		if (!isIPAddressValid()) {
+			console.log(ipAddress);
+			return;
+		}
 
-    try {
-      const response: AxiosResponse<IPFY> = await axios.get(
-        `${IP_GEOLOCATION_COUNTRY_API}?apiKey=${
-          import.meta.env.VITE_IPFY_API_KEY
-        }&ipAddress=${ipAddress}`
-      );
+		try {
+			const response: AxiosResponse<IPFY> = await axios.get(
+				`${IP_GEOLOCATION_COUNTRY_API}?apiKey=${
+					import.meta.env.VITE_IPFY_API_KEY
+				}&ipAddress=${ipAddress}`
+			);
 
-      if (response.status === 200) {
-        setResult(response.data);
-      } else {
-        throw new Error(`API returned status code ${response.status}`);
-      }
-    } catch (error) {
-      console.error(`Error fetching user data: ${error}`);
-      throw error;
-    }
-  };
+			if (response.status === 200) {
+				setResult(response.data);
+			} else {
+				throw new Error(`API returned status code ${response.status}`);
+			}
+		} catch (error) {
+			console.error(`Error fetching user data: ${error}`);
+			throw error;
+		}
+	};
 
-  useEffect(() => {
-    if (ipAddress === undefined) return;
-    fetchIPGeoLocation();
-  }, [ipAddress]);
+	useEffect(() => {
+		if (ipAddress === undefined) return;
+		fetchIPGeoLocation();
+	}, [ipAddress]);
 
-  return { result };
+	return { result };
 };
